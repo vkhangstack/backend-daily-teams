a59a7edb2cf7ff4231d5bcc531c7e57138af0c4f
Pham Van Khang <phamvankhang.tvi@gmail.com>
2025-05-08 23:25:46 +0700

    chore: update task dto

--- internal/core/domain/dto/daily.go
+++ internal/core/domain/dto/daily.go
@@ -1,26 +1,24 @@
 package dto
 
 type CreateDailyDto struct {
-	Title           string  `json:"title"`
-	Content         string  `json:"content"`
-	Start           string  `json:"start"`
-	End             string  `json:"end"`
-	TextColor       string  `json:"textColor,omitempty"`
-	BackgroundColor string  `json:"backgroundColor,omitempty"`
-	IsDaily         *bool   `json:"isDaily,omitempty"`
-	AllDay          *bool   `json:"allDay" default:"false"`
-	Token           *string `json:"token,omitempty"`
+	Title           string `json:"title"`
+	Content         string `json:"content"`
+	Start           string `json:"start"`
+	End             string `json:"end"`
+	TextColor       string `json:"textColor,omitempty"`
+	BackgroundColor string `json:"backgroundColor,omitempty"`
+	IsDaily         *bool  `json:"isDaily,omitempty"`
+	AllDay          *bool  `json:"allDay" default:"false"`
 }
 
 type UpdateDailyDto struct {
-	ID              string  `json:"id"`
-	Content         string  `json:"content,omitempty"`
-	Title           string  `json:"title,omitempty"`
-	Start           string  `json:"start,omitempty"`
-	End             string  `json:"end,omitempty"`
-	TextColor       string  `json:"textColor,omitempty"`
-	BackgroundColor string  `json:"backgroundColor,omitempty"`
-	IsDaily         *bool   `json:"isDaily,omitempty"`
-	AllDay          *bool   `json:"allDay,omitempty"`
-	Token           *string `json:"token,omitempty"`
+	ID              string `json:"id"`
+	Content         string `json:"content,omitempty"`
+	Title           string `json:"title,omitempty"`
+	Start           string `json:"start,omitempty"`
+	End             string `json:"end,omitempty"`
+	TextColor       string `json:"textColor,omitempty"`
+	BackgroundColor string `json:"backgroundColor,omitempty"`
+	IsDaily         *bool  `json:"isDaily,omitempty"`
+	AllDay          *bool  `json:"allDay,omitempty"`
 }
